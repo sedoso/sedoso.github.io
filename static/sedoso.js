c715d5ca174a7c0464030329b077be8aef495f72
digitalBeing <alohanvasu@gmail.com>
2018-02-18 19:06:24 +0530

    parallax + sticky Meny + real cool looking site

--- static/sedoso.js
+++ static/sedoso.js
@@ -1,8 +1,8 @@
-// Written at BinaryBlood
+/*// Written at BinaryBlood
 $(window).scroll(function() {
     if ($("nav").offset().top > 50) {
         $("nav").removeClass('no-bg navbar-dark').addClass("navbar-light bg-light");
     } else {
     	$("nav").removeClass("navbar-light bg-light").addClass('no-bg navbar-dark');    
     }   
-});
+});*/
